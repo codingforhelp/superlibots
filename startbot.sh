033faf55c4a5469973f6d16db8b3337886d2ab8a
RPNS Nimsilu <93962127+codingforhelp@users.noreply.github.com>
2021-12-12 00:11:33 +0530

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-FROM debian:stable-slim
-MAINTAINER OIVAS7572
-RUN echo OIVAS7572
-CMD echo OIVAS7572
-COPY . .
-
-#ADD /engine/ .
-#RUN rm -r engine
-
-# If you want to run any other commands use "RUN" before.
-
-RUN apt-get update && apt-get install -y wget python3 python3-pip p7zip-full
-
-RUN wget --no-check-certificate "https://gitlab.com/OIVAS7572/Goi5.1.bin/-/raw/master/Goi5.1.bin.7z" -O Goi5.1.bin.7z
-RUN 7z e Goi5.1.bin.7z
-RUN rm Goi5.1.bin.7z
-RUN wget --no-check-certificate "https://gitlab.com/OIVAS7572/Cerebellum3merge.bin/-/raw/master/Cerebellum3Merge.bin.7z" -O Cerebellum3Merge.bin.7z
-Run 7z e Cerebellum3Merge.bin.7z
-RUN rm Cerebellum3Merge.bin.7z
-RUN wget --no-check-certificate "https://abrok.eu/stockfish/builds/2807dcfab671bfc7a1bea79f5639dbbd505703ad/linux64modern/stockfish_21083112_x64_modern.zip" -O chess-engine.zip
-#RUN wget --no-check-certificate "https://data.stockfishchess.org/nn/nn-938525872610.nnue" -O nn-938525872610.nnue
-RUN 7z e chess-engine.zip && rm chess-engine.zip && mv stockfish* chess-engine
-chmod +x engines/chess-engine
+chmod +x engines/stockfish_14
+python lichess-bot.py -u
